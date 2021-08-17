b41f7dedf48093e6607b65827da79c097e80afd6
Diogo Medeiros <sr.diogo@gmail.com>
2021-08-17 15:04:39 -0300

    Exercicio 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,5 +1,9 @@
 import './style.css'
 const app = document.querySelector('[data-js="app"]')
+const button = document.querySelector('[data-js="button"]')
+button.addEventListener('click', () => {
+  app.style.display === 'none' ? app.style.display = 'block' : app.style.display = 'none'
+})
 app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
